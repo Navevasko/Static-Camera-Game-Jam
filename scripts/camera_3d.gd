11b355e47cea45f52664b91e15badcd2c882925b
Guilherme Navevaiko <guilherme.navevaiko@hotmail.com>
2025-07-07 14:48:29 -0300

    feat: adicionando partículas de neve e arrumando câmera fixa

--- scripts/camera_3d.gd
+++ scripts/camera_3d.gd
@@ -3,20 +3,16 @@ extends Camera3D
 # EXPORTED VARIABLES
 @export var is_default: bool = false
 
-var player: Player
-
-func _physics_process(delta: float) -> void:
-	if player:
-		look_at(player.position)
+func _ready() -> void:
+	current = is_default
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	print(body)
+	print(body is Player)
 	if body is Player:
 		print(name + " Está focando no player")
 		current = true
-		player = body
 
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
-	print(name + " Está não está mais focando no player")
+	print(name + " Não está mais focando no player")
 	current = false
